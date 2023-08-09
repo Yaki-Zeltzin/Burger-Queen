bf8e2860ce2e1da47c7529f7766882361bc09d06
alison-leodra <127059922+alison-leodra@users.noreply.github.com>
2023-08-09 09:45:47 -0400

    intento hu fail

--- src/components/pages/chef/Product.jsx
+++ src/components/pages/chef/Product.jsx
@@ -1,17 +1,34 @@
-const Product = ({id, name, image, price, type}) => {
+import PageWaiters from "../waiter/PageWaiters"
+import React from "react"
+import { createContext } from "react"
+import { useState } from "react"
 
+const choosenProduct = React.createContext();
+
+const Product = ({ id, name, image, price, type }) => {
+
+    const [product, setProduct] = useState([0])
+
+
+    const addProduct = (e) => {
+        const idProduct = e.target.id;
+        setProduct(product.concat(idProduct))
+        console.log("id de producto  " + idProduct)
+        console.log(product)
+
+    }
+
+    // console.log(product)
     return (
-        <div className="product" key={id}>
-            <img className="img-products" 
-            src={image} 
-            alt={name} 
-            id={id}   
-            onClick={(e) => {
-                console.log(e.target.id)
-            }}
+        <div className="product" key={id} >
+            <img className="img-products"
+                src={image}
+                alt={name}
+                id={id}
+                onClick={addProduct}
             />
             <p className="item-product">Producto: {name}
-            <span> ${price}</span></p>
+                <span> ${price}</span></p>
         </div>
     )
 
